7759cf9be3a027c646431b6a7783d8070295e1de
Jonathan Slenders <jonathan.slenders@mobilevikings.com>
2011-12-08 12:57:37 +0100

    moved django_votes of of src, added version to __init__

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
+import django_votes
 
 setup(
     name="django-votes",
+    version=django_votes.__version__,
     url='https://github.com/citylive/django-votes',
     license='BSD',
     description="Voting system for various things",
     long_description=open('README', 'r').read(),
     author='Maarten Timmerman, City Live nv',
-    packages=find_packages('src'),
+    packages=find_packages('.'),
       # package_data={'django_votes': [
       #             'static/*/*/*.js',
       #             'static/*/*/*.css',
@@ -16,7 +18,7 @@ setup(
       #             ], },
     zip_safe=False, # Don't create egg files, Django cannot find templates in egg files.
     include_package_data=True,
-    package_dir={'': 'src'},
+    package_dir={'': '.'},
     classifiers=[
         'Intended Audience :: Developers',
         'Programming Language :: Python',
